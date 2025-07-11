3144c7fab9972f380530e215a26f530ea1edd871
zhangyu <zhangyu1@bjneweastech.com>
2025-07-11 09:43:34 +0800

    新增CMakelist

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtChess_FILE D:/workspace/qtchess/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/bin/QtChess.exe)
-set(__QT_DEPLOY_TARGET_QtChess_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtChess_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
